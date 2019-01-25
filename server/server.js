aea9bc9d03373bb10a2becbe74a2ae0814e20d4b
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-25 15:50:02 -0500

    Added socket.io and basic connection io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
-
+const http = require('http');
 var express = require('express');
+const socketIO = require('socket.io');
 
-var app = express();
+const publicPath = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000;
-app.use(express.static(publicPath))
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port, () => {
+app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log('New User connected')
+  socket.on('disconnect', () => {
+    console.log('Disconnected from user')
+  })
+});
+
+
+
+server.listen(port, () => {
   console.log(`Listening to port ${port}`)
 })
