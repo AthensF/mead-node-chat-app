01422133650780cd6951ef4b8e2d7a3244e731b7
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-30 18:25:59 -0500

    added form submission to client

--- server/server.js
+++ server/server.js
@@ -1,36 +1,40 @@
 const path = require('path');
 const http = require('http');
-var express = require('express');
+const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
-io.on('connection', (socket) => { //event handlers
-  console.log('New User connected')
 
-  socket.emit('newMessage',generateMessage("Admin", "Welcome to the app"));
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-  socket.broadcast.emit('newMessage', generateMessage("Admin", 'New user joined'))
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
   socket.on('disconnect', () => {
-    console.log('Disconnected from user')
-  })
-
-  socket.on('createMessage', (message) => {
-    console.log('newMessage', message);
-
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    console.log('User was disconnected');
   });
 });
 
-
-
 server.listen(port, () => {
-  console.log(`Listening to port ${port}`)
-})
+  console.log(`Server is up on ${port}`);
+});
